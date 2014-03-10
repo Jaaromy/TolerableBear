a8cc242fcc561c80e4fcbaa5c550a42ee46f6372
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-03-10 15:19:45 -0600

    Fixed layer list memory leak.

--- TolerableBear/app/services/agentservice.js
+++ TolerableBear/app/services/agentservice.js
@@ -5,13 +5,56 @@
    var agents = [];
    var layers = [];
 
+   function removeLayer(agent) {
+      for (var i = 0; i < layers.length; i++) {
+         for (var j = 0; j < layers[i].children.length; j++) {
+            if (layers[i].children[j].aname && layers[i].children[j].aname === agent.aname) {
+               //Remove layer from stage.
+               layers[i].remove();
+               //Remove layer from list.
+               layers.splice(i, 1);
+               return;
+            }
+         }
+      }
+   }
+
+   function removeAgent(agent) {
+      if (agent && agent.aname) {
+         //Remove from layer.
+         agent.remove();
+         for (var i = 0; i < agents.length; i++) {
+            if (agents[i].aname === agent.aname) {
+               agents.splice(i, 1);
+               return;
+            }
+         }
+      }
+   }
+
+   function addAgent(agent) {
+      if (agent) {
+         removeAgent(agent);
+         agents.push(agent);
+         return agent;
+      }
+   }
+
+   function addLayer(layer, agent) {
+      if (layer) {
+         removeLayer(agent);
+         layers.push(layer);
+      }
+   }
+
    AgentServiceFactory.createAgent = function (stage, name) {
       var agent = KineticService.circle(50,50, 15);
       agent.aname = name ? name : layers.length + 1;
-      var layer = KineticService.layer(agent);
-      stage.add(layer);
 
-      agents.push(agent);
+      addAgent(agent);
+      var layer = KineticService.layer(agent);
+      addLayer(layer, agent);
+      stage.add(layer);
       layers.push(layer);
    };
 
