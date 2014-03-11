bb6b6c3386241b949b9313b7d2301828b55e18a7
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-03-11 11:52:02 -0600

    Width now set dynmically based on window width.

--- TolerableBear/app/controllers/circlecontroller.js
+++ TolerableBear/app/controllers/circlecontroller.js
@@ -1,15 +1,22 @@
 ﻿angular.module('MyModule')
-    .controller('circlecontroller', function ($scope, $timeout, KineticService, UtilityService, AgentService) {
+    .controller('circlecontroller', function ($scope, $window, $timeout, KineticService, UtilityService, AgentService) {
        'use strict';
+       $scope.windowWidth = function () {
+          return $window.innerWidth;
+       }
+
+       $scope.$watch($scope.windowWidth, function (newValue, oldValue) {
+          stage.setWidth(newValue);
+       });
 
        var stage = {};
        function init() {
           $scope.pageName = "CIRCLES";
-          stage = KineticService.createStage('container', 1024, 768);
+          stage = KineticService.createStage('container', $scope.windowWidth(), 768);
           AgentService.clear();
 
           for (var i = 0; i < 80; i++) {
-             var v1 = new Vec2(UtilityService.randomInt( -10, 10), UtilityService.randomInt( -10, 10));
+             var v1 = new Vec2(UtilityService.randomInt(-10, 10), UtilityService.randomInt(-10, 10));
              v1.normalize();
              AgentService.createAgent(stage, null, v1, UtilityService.randomInt(5, 30));
           }
