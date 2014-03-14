de331fb2699c10275adcd90313dc603cb47d9483
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-03-13 22:41:41 -0600

    Add css and controller files.

---
+++ TolerableBear/app/controllers/navcontroller.js
@@ -0,0 +1,10 @@
+﻿angular.module('MyModule')
+    .controller('navcontroller', function ($scope, $location) {
+        'use strict';
+
+        $scope.navModel = 'main';
+
+        $scope.$watch('navModel', function (newValue, oldValue) {
+            $location.path('/' + newValue);
+        });
+    });
