9ec64e2a4e1f00166b0e915ccbb8e01415fe74dd
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-03-14 12:04:17 -0600

    Auto creation of agents in velocity controller.

--- TolerableBear/app/controllers/velocitycontroller.js
+++ TolerableBear/app/controllers/velocitycontroller.js
@@ -2,16 +2,16 @@
     .controller('velocitycontroller', function ($scope, $window, $timeout, KineticService, UtilityService, AgentService) {
         'use strict';
         $scope.windowWidth = function () {
-            return 1000;
+            return $window.innerWidth;
         };
 
         //$scope.windowHeight = function () {
         //   return $window.innerHeight;
         //}
 
-        //$scope.$watch($scope.windowWidth, function (newValue, oldValue) {
-        //    stage.setWidth(newValue);
-        //});
+        $scope.$watch($scope.windowWidth, function (newValue, oldValue) {
+            stage.setWidth(newValue);
+        });
 
         //$scope.$watch($scope.windowHeight, function (newValue, oldValue) {
         //   stage.setHeight(newValue < 300 ? 300 : newValue);
@@ -27,36 +27,15 @@
             var radius = 25;
             var diam = radius * 2;
             var v1 = {};
+            var count = 0;
 
-            v1 = new Vec2(1, 0);
-            v1 = vMath.normalize(v1);
-            v1 = vMath.mulS(v1, 1);
-            AgentService.createAgent(stage, null, new Vec2(radius, diam), v1, radius);
-
-            v1 = new Vec2(1, 0);
-            v1 = vMath.normalize(v1);
-            v1 = vMath.mulS(v1, 5);
-            AgentService.createAgent(stage, null, new Vec2(radius, diam * 2), v1, radius);
-
-            v1 = new Vec2(1, 0);
-            v1 = vMath.normalize(v1);
-            v1 = vMath.mulS(v1, 10);
-            AgentService.createAgent(stage, null, new Vec2(radius, diam * 3), v1, radius);
-
-            v1 = new Vec2(1, 0);
-            v1 = vMath.normalize(v1);
-            v1 = vMath.mulS(v1, 20);
-            AgentService.createAgent(stage, null, new Vec2(radius, diam * 4), v1, radius);
-
-            v1 = new Vec2(1, 0);
-            v1 = vMath.normalize(v1);
-            v1 = vMath.mulS(v1, 15);
-            AgentService.createAgent(stage, null, new Vec2(radius, diam * 5), v1, radius);
-
-            v1 = new Vec2(1, 0);
-            v1 = vMath.normalize(v1);
-            v1 = vMath.mulS(v1, 7);
-            AgentService.createAgent(stage, null, new Vec2(radius, diam * 6), v1, radius);
+            while (count * diam < stage.getHeight()) {
+                count++;
+                v1 = new Vec2(1, 0);
+                v1 = vMath.normalize(v1);
+                v1 = vMath.mulS(v1, count * count);
+                AgentService.createAgent(stage, null, new Vec2(radius, diam * count), v1, radius);
+            }
         }
 
         init();
