ff69e44767ef8abaf9f01ccc9a6aa008688b89c6
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-05-05 09:46:52 -0600

    Center canvas. Clean up main circle creation and references.

--- app/controllers/phasercollisioncontroller.js
+++ app/controllers/phasercollisioncontroller.js
@@ -1,8 +1,6 @@
 angular.module('MyModule')
     .controller('phasercollisioncontroller', function ($scope, $window, $timeout, UtilityService) {
         'use strict';
-        $scope.myfps;
-        
         $scope.windowWidth = function () {
             return $window.innerWidth;
         };
@@ -12,7 +10,7 @@ angular.module('MyModule')
             angular.element(document.querySelectorAll('canvas')).remove();
         });
         
-        var game = new $window.Phaser.Game($window.innerWidth - 40, 600, $window.Phaser.AUTO, '', { preload: preload, create: create, update: update });
+        var game = new $window.Phaser.Game(600, 600, $window.Phaser.AUTO, 'container', { preload: preload, create: create, update: update });
         var fpsText;
         var avgFpsText;
         var ballCountText;
@@ -23,7 +21,8 @@ angular.module('MyModule')
         var fpsToggle = false;
         var colorSwapToggle = false;
         var whiteToggle = false;
-        //var radius = 25;
+        var mainCircle1;
+        var mainCircle2;
 
         function preload() {
             game.load.image('agent', 'assets/circle.png');
@@ -45,7 +44,7 @@ angular.module('MyModule')
             
             game.input.touch.onTouchEnd = function(e) {
                 fpsToggle = !fpsToggle;
-            }
+            };
             
             game.time.advancedTiming = true;
             game.physics.startSystem(Phaser.Physics.P2JS);
@@ -57,69 +56,24 @@ angular.module('MyModule')
             circles = game.add.group();
             circles.enableBody = true;
             circles.physicsBodyType = Phaser.Physics.P2JS;
+            
+            mainCircle1 = createMainCircle(0, 0);
+            mainCircle2 = createMainCircle(game.world.width, game.world.height);
 
             fpsText = game.add.text(16, 14, 'FPS: 0', { font: 'bold 14px Arial', fill: '#ff0000' });
             avgFpsText = game.add.text(16, 28, 'AVG FPS: 0', { font: 'bold 14px Arial', fill: '#ff0000' });
             ballCountText = game.add.text(16, 42, 'BALL COUNT: 0', { font: 'bold 14px Arial', fill: '#ff0000' });
-
-//            var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial');
-
-//            var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
-            //  4 trues = the 4 faces of the world in left, right, top, bottom order
-            //game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
-
-            //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
-            //  those 2 materials collide it uses the following settings.
-            //  A single material can be used by as many different sprites as you like.
-//            var contactMaterial = game.physics.p2.createContactMaterial(spriteMaterial, worldMaterial);
-//            contactMaterial.friction = 0.0;     // Friction to use in the contact of these two materials.
-//            contactMaterial.restitution = 1000.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-//            contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-//            contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-//            contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-//            contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-//            contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
-
-            for (var i = 0; i < 50; i++)
-            {
-                //createCircle();
-//                var radius = UtilityService.randomInt(8,10);
-//                var circle = circles.create(game.world.randomX, game.world.randomY, 'agent');
-//                if (i < 2) {
-//                    circle.width = 120;
-//                    circle.height = 120;
-//                    circle.body.setCircle(60,0,0,0);
-//                    circle.body.mass = 5000;
-//
-//                    //circle.body.setMaterial(spriteMaterial);
-//                    //circle.body.setZeroDamping();
-//                    circle.body.setCollisionGroup(circleCollisionGroup);
-//                    circle.body.collides(circleCollisionGroup);
-//                    circle.body.angle = UtilityService.randomInt(0,359);
-//                    circle.body.moveForward(UtilityService.randomInt(500,750));
-//                } else {
-//                    circle.width = radius * 2;
-//                    circle.height = circle.width;
-//                    circle.body.setCircle(radius,0,0,0);
-//
-//                    //circle.body.setMaterial(spriteMaterial);
-//                    circle.body.setCollisionGroup(circleCollisionGroup);
-//                    circle.body.collides(circleCollisionGroup);
-//                    circle.body.angle = UtilityService.randomInt(0,359);
-//                    //circle.body.moveForward(UtilityService.randomInt(500,1000));
-//                    circle.body.mass = radius;
-//                }
-            }
-            
-            //circle.body.moveForward(1000);
-            
         }
         
         function circleContact(a, b, c, d) {
             if (whiteToggle && b.collisionGroup == c.collisionGroup) {
                 d[0].bodyA.parent.sprite.tint = 0xffffff;
             } 
-            else if (colorSwapToggle && b.collisionGroup == c.collisionGroup && (d[0].bodyA.id != 5 && d[0].bodyB.id != 5 && d[0].bodyA.id != 6 && d[0].bodyB.id != 6 )) {
+            else if (colorSwapToggle && b.collisionGroup == c.collisionGroup && 
+                     (d[0].bodyA.id != mainCircle1.body.id && 
+                      d[0].bodyB.id != mainCircle1.body.id && 
+                      d[0].bodyA.id != mainCircle2.body.id && 
+                      d[0].bodyB.id != mainCircle2.body.id )) {
                 var v1 = new Vec2(d[0].bodyA.velocity[0], d[0].bodyA.velocity[1]);
                 var v2 = new Vec2(d[0].bodyB.velocity[0], d[0].bodyB.velocity[1]);
                 var len1 = vMath.length(v1);
@@ -129,6 +83,33 @@ angular.module('MyModule')
                 } else if (len2 > len1 && len2 > 6) {
                     d[0].bodyA.parent.sprite.tint = d[0].bodyB.parent.sprite.tint;
                 }
+            }
+        }
+        
+        function createMainCircle(x, y) {
+            var circle;
+            ballCount++;
+            circle = circles.create(x, y, 'agent');
+            circle.width = 100;
+            circle.height = 100;
+            circle.body.setCircle(50,0,0,0);
+            circle.body.mass = 5000;
+            circle.tint = 0xffffff;
+
+            circle.body.setCollisionGroup(circleCollisionGroup);
+            circle.body.collides(circleCollisionGroup);
+            circle.body.angle = UtilityService.randomInt(0,359);
+            
+            return circle;
+        }
+        
+        function accelerateSlow(circle) {
+            if (circle.body.velocity.x > -2.0 && 
+                circle.body.velocity.x < 2.0 && 
+                circle.body.velocity.y > -2.0 && 
+                circle.body.velocity.y < 2.0) {
+                circle.body.angle = UtilityService.randomInt(0,359);
+                circle.body.moveForward(UtilityService.randomInt(200,500));
             }
         }
         
@@ -156,42 +137,27 @@ angular.module('MyModule')
                   circle = circles.create(game.world.width, 0, 'agent');
                   circle.tint = whiteToggle ? 0xffffff : 0xff0000;
             }
-                
             
-            if (circles.children.length <= 2) {
-                circle.width = 120;
-                circle.height = 120;
-                circle.body.setCircle(60,0,0,0);
-                circle.body.mass = 5000;
-                circle.tint = 0xffffff;
+            circle.width = radius * 2;
+            circle.height = circle.width;
+            circle.body.setCircle(radius,0,0,0);
 
-                //circle.body.setMaterial(spriteMaterial);
-                //circle.body.setZeroDamping();
-                circle.body.setCollisionGroup(circleCollisionGroup);
-                circle.body.collides(circleCollisionGroup);
-                circle.body.angle = UtilityService.randomInt(0,359);
-                //circle.body.moveForward(UtilityService.randomInt(500,750));
-            } else {
-                circle.width = radius * 2;
-                circle.height = circle.width;
-                circle.body.setCircle(radius,0,0,0);
-                
-                circle.body.onBeginContact.add(circleContact, this);
+            circle.body.onBeginContact.add(circleContact, this);
 
-                //circle.body.setMaterial(spriteMaterial);
-                circle.body.setCollisionGroup(circleCollisionGroup);
-                circle.body.collides(circleCollisionGroup);
-                circle.body.angle = UtilityService.randomInt(0,359);
-                //circle.body.moveForward(UtilityService.randomInt(500,1000));
-                circle.body.mass = radius;
-            }
+            circle.body.setCollisionGroup(circleCollisionGroup);
+            circle.body.collides(circleCollisionGroup);
+            circle.body.angle = UtilityService.randomInt(0,359);
+            circle.body.mass = radius;
         }
         
         function destroyCircle() {
             var circle = circles.children[circles.children.length - 1];
-            circle.body.destroy();
-            circle.destroy();
-            ballCount--;
+            
+            if (circle.body.id != mainCircle1.body.id && circle.body.id != mainCircle2.body.id) {
+                circle.body.destroy();
+                circle.destroy();
+                ballCount--;
+            }
         }
 
         var frameCount = 0;
@@ -216,24 +182,16 @@ angular.module('MyModule')
                 ballCountText.text = '';
             }
             
-            if ((avgFps >= 60 && game.time.fps >= 60 && ballCount < 1400) || ballCount < 2) {
+            if ((avgFps >= 60 && game.time.fps >= 60 && ballCount < 1400) || ballCount < 20) {
                 createCircle();
             }
             
-            if (avgFps <= 55 && game.time.fps <= 55 && ballCount > 50) {
+            if (avgFps <= 55 && game.time.fps <= 55 && ballCount > 100) {
                 destroyCircle();
             }
-            
-            if (ballCount >= 2) {
-                for (var i = 0; i < 2; i++)
-                {
-                    var circle = circles.children[i];
-                    if (circle.body.velocity.x > -2.0 && circle.body.velocity.x < 2.0 && circle.body.velocity.y > -2.0 && circle.body.velocity.y < 2.0) {
-                        circle.body.angle = UtilityService.randomInt(0,359);
-                        circle.body.moveForward(UtilityService.randomInt(200,500));
-                    }
-                }
-            }
+
+            accelerateSlow(mainCircle1);
+            accelerateSlow(mainCircle2);
         }
         
         function init() {
