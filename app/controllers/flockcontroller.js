5b96c3a4a5843593bb3ef0be21fbdaa250ad6f31
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-05-07 10:51:12 -0600

    Set flock link to in progress. Add target and multiple agents.

--- app/controllers/flockcontroller.js
+++ app/controllers/flockcontroller.js
@@ -14,23 +14,57 @@ angular.module('MyModule')
 
         function preload() {
             game.load.image('agent', 'assets/triangle.png');
+            game.load.image('target', 'assets/circle.png');
         }
 
-        var agent;
-        var statsText;
+        var agents;
+        var target;
+        var agentCollisionGroup;
+        
+        function createAgent(x, y) {
+            var agent;
+            agent = agents.create(x, y, 'agent');
+            agent.width = 20;
+            agent.height = 20;
+            agent.anchor.setTo(0.5, 1.0);
+
+            agent.body.setCollisionGroup(agentCollisionGroup);
+//            agent.body.collides(circleCollisionGroup);
+//            agent.body.angle = UtilityService.randomInt(0,359);
+            
+            return agent;
+        }
         
         function create() {
             game.physics.startSystem(Phaser.Physics.P2JS);
-            agent = game.add.sprite(game.world.width / 2, game.world.height / 2, 'agent');
-            agent.anchor.setTo(0.5, 1.0);
-            agent.width = 20;
-            agent.height = 20;
+            agents = game.add.group();
+            agents.enableBody = true;
+            agents.physicsBodyType = Phaser.Physics.P2JS;
             
-            game.physics.enable(agent, Phaser.Physics.P2JS);
-            agent.body.moveForward(500);
+            agentCollisionGroup = game.physics.p2.createCollisionGroup();
+            
+            target = game.add.sprite(300,300,'target');
+            target.width = 15;
+            target.height = target.width;
+            target.anchor.setTo(0.5, 0.5);
+            target.tint = 0x00ffff;
+            game.physics.enable(target, Phaser.Physics.P2JS);
+            
+            createAgent(100,100);
+            createAgent(200,200);
+            createAgent(300,300);
+            createAgent(400,400);
+        }
+        
+        function move(agent) {
+            agent.body.moveForward(100);
+            agent.body.rotateRight(30);
         }
         
         function update() {
+            for(var i = 0; i < agents.children.length; i++) {
+                move(agents.children[i]);
+            }
         }
         
         function init() {
