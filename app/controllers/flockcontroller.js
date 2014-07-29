466113295b0ab6fa4e06ff355917cc66eb0aa2f4
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2014-07-29 13:51:20 -0600

    Fewer agents.

--- app/controllers/flockcontroller.js
+++ app/controllers/flockcontroller.js
@@ -59,7 +59,7 @@ angular.module('MyModule')
             game.physics.p2.enable(target);
             target.body.setCollisionGroup(targetCollisionGroup);
             
-            for(var i = 0; i < 1000; i++) {
+            for(var i = 0; i < 400; i++) {
                 createAgent(UtilityService.randomInt(50, 550), UtilityService.randomInt(50, 550));
             }
             
